c2a9f7da2731757488399ae2ebd826afd8a72bc0
Daniel Benedí García <danielbenedigarcia@gmail.com>
2023-10-24 13:11:30 +0200

    Setup code for printing tables

--- assignment4/assignment4.R
+++ assignment4/assignment4.R
@@ -19,6 +19,21 @@ generate_table_entry <- function(language, dataset) {
           )
 }
 
+print_row_type2 <- function(language, model0, model1, model2, model3, model4, model1p, model2p, model3p, model4p){
+  sprintf(r"( %s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\)",
+          language,
+          model0,  # Model 0
+          model1,  # Model 1
+          model2,  # Model 2
+          model3,  # Model 3
+          model4,  # Model 4
+          model1p, # Model 1+
+          model2p, # Model 2+
+          model3p, # Model 3+
+          model4p  # Model 4+
+  )
+}
+
 model1 <- function(n,b) {
   (n/2)**b
 }
@@ -73,8 +88,43 @@ languages = c("Arabic", "Basque", "Catalan",
               "Chinese", "Czech", "English",
               "Greek", "Hungarian", "Italian",
               "Turkish")
+table_summary <- r"(\begin{table}[!htb]
+\centering
+\resizebox{\columnwidth}{!}{
+\begin{tabular}{llllll}{
+Language & N & $\mu_n$ & $\sigma_n$ & $\mu_x$ & $\sigma_x$ \\ \hline
+)"
+table_res_se <- r"(\begin{table}[!htb]
+\centering
+\resizebox{\columnwidth}{!}{
+\begin{tabular}{llllllllll}{
+Language & 0 & 1 & 2 & 3 & 4 & 1+ & 2+ & 3+ & 4+ \\ \hline
+)"
+table_aic <- r"(\begin{table}[!htb]
+\centering
+\resizebox{\columnwidth}{!}{
+\begin{tabular}{llllllllll}{
+Language & 0 & 1 & 2 & 3 & 4 & 1+ & 2+ & 3+ & 4+ \\ \hline
+)"
+table_aic_diff <- r"(\begin{table}[!htb]
+\centering
+\resizebox{\columnwidth}{!}{
+\begin{tabular}{llllllllll}{
+Language & 0 & 1 & 2 & 3 & 4 & 1+ & 2+ & 3+ & 4+ \\ \hline
+)"
+table_params <- r"(\begin{table}[!htb]
+\centering
+\resizebox{\columnwidth}{!}{
+\begin{tabular}{lllllllllllllllll}{
+         & \multicolumn{16}{l}{Model} \\ \cline{2-17} 
+         & \multicolumn{1}{l|}{1} & \multicolumn{2}{l|}{2}     & \multicolumn{2}{l|}{3}     & \multicolumn{1}{l|}{4} & \multicolumn{2}{l|}{1+}    & \multicolumn{3}{l|}{2+}        & \multicolumn{3}{l|}{3+}        & \multicolumn{2}{l}{4+} \\
+Language & \multicolumn{1}{l|}{b} & a & \multicolumn{1}{l|}{b} & a & \multicolumn{1}{l|}{c} & \multicolumn{1}{l|}{a} & b & \multicolumn{1}{l|}{d} & a & b & \multicolumn{1}{l|}{d} & a & c & \multicolumn{1}{l|}{d} & a & d \\ \hline
+)"
+
 for(language in languages){
   data = read_dataset(language)
+  table_summary <- paste(table_summary,generate_table_entry(language,data), sep="\n")
+  
   means = aggregate(data, list(data$vertices), mean)
   
   plt <- ggplot() + geom_point(data=dataset, aes(x=vertices,y=mean_length))
@@ -85,5 +135,63 @@ for(language in languages){
   plt <- plt + xlab("Number of vertices")
   plt <- plt + ylab("Mean length")
   
-  # lines(log(mean_Catalan$vertices),log((mean_Catalan$vertices+1)/3), col = "red")
+  table_res_se <- paste(table_res_se, print_row_type2(language,
+                                                      0., # Model 0
+                                                      0., # Model 1
+                                                      0., # Model 2
+                                                      0., # Model 3
+                                                      0., # Model 4
+                                                      0., # Model 1+
+                                                      0., # Model 2+
+                                                      0., # Model 3+
+                                                      0.  # Model 4+
+  ), sep="\n")
+  table_aic <- paste(table_aic, print_row_type2(language,
+                                                      0., # Model 0
+                                                      0., # Model 1
+                                                      0., # Model 2
+                                                      0., # Model 3
+                                                      0., # Model 4
+                                                      0., # Model 1+
+                                                      0., # Model 2+
+                                                      0., # Model 3+
+                                                      0.  # Model 4+
+  ), sep="\n")
+  table_aic_diff <- paste(table_aic_diff, print_row_type2(language,
+                                                      0., # Model 0
+                                                      0., # Model 1
+                                                      0., # Model 2
+                                                      0., # Model 3
+                                                      0., # Model 4
+                                                      0., # Model 1+
+                                                      0., # Model 2+
+                                                      0., # Model 3+
+                                                      0.  # Model 4+
+  ), sep="\n")
+  table_params <- paste(table_params, sprintf(r"( %s & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f \\)",
+                                              language,
+                                              param_b_model1,
+                                              param_a_model2, param_b_model2,
+                                              param_a_model3, param_c_model3,
+                                              param_a_model4,
+                                              param_b_model1p, param_d_model1p,
+                                              param_a_model2p, param_b_model2p, param_d_model2p,
+                                              param_a_model3p, param_c_model3p, param_d_model3p,
+                                              param_a_model4p, param_d_model4p
+  ), sep="\n")
 }
+table_summary <- paste(table_summary, r"(\end{tabular}}
+\caption{Summary of the properties of the degree sequences \label{tab:summary}}
+\end{table})",sep="\n")
+table_res_se <- paste(table_res_se, r"(\end{tabular}}
+\caption{Residual standard error for each model \label{tab:res_se}}
+\end{table})",sep="\n")
+table_aic <- paste(table_aic, r"(\end{tabular}}
+\caption{Akaike information criterion (AIC) of each model \label{tab:aic}}
+\end{table})",sep="\n")
+table_aic_diff <- paste(table_aic_diff, r"(\end{tabular}}
+\caption{AIC differences \label{tab:aic_diff}}
+\end{table})",sep="\n")
+table_params <- paste(table_params, r"(\end{tabular}}
+\caption{Parameters of each fitted model \label{tab:params}}
+\end{table})",sep="\n")
