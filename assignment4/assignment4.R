078353a84e6df7ad2d6a826a908391e79bfd6611
Daniel Benedí García <danielbenedigarcia@gmail.com>
2023-11-08 11:46:52 +0100

    Added model 5 y 5+

--- assignment4/assignment4.R
+++ assignment4/assignment4.R
@@ -11,7 +11,7 @@ read_dataset <- function(language) {
 generate_table_entry <- function(language, dataset) {
   sprintf(r"(%s & %d & %.3f & %.3f & %.3f & %.3f \\)",
           language,
-          length(dataset),
+          nrow(dataset),
           mean(dataset$vertices),
           sd(dataset$vertices),
           mean(dataset$mean_length),
@@ -19,18 +19,20 @@ generate_table_entry <- function(language, dataset) {
           )
 }
 
-print_row_type2 <- function(language, model0, model1, model2, model3, model4, model1p, model2p, model3p, model4p){
-  sprintf(r"( %s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\)",
+print_row_type2 <- function(language, model0, model1, model2, model3, model4, model5, model1p, model2p, model3p, model4p, model5p){
+  sprintf(r"( %s & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f & %.3f \\)",
           language,
           model0,  # Model 0
           model1,  # Model 1
           model2,  # Model 2
           model3,  # Model 3
           model4,  # Model 4
+          model5,  # Model 4
           model1p, # Model 1+
           model2p, # Model 2+
           model3p, # Model 3+
-          model4p  # Model 4+
+          model4p,  # Model 4+
+          model5p  # Model 5+
   )
 }
 
@@ -40,35 +42,35 @@ languages = c("Arabic", "Basque", "Catalan",
               "Turkish")
 table_summary <- r"(\begin{table}[!htb]
 \centering
-\resizebox{\columnwidth}{!}{
+%\resizebox{\columnwidth}{!}{
 \begin{tabular}{llllll}
-Language & N & $\mu_n$ & $\sigma_n$ & $\mu_x$ & $\sigma_x$ \\ \hline
+Language & N & $\mu_n$ & $\sigma_n$ & $\mu_{\langle d \rangle}$ & $\sigma_{\langle d \rangle}$ \\ \hline
 )"
 table_res_se <- r"(\begin{table}[!htb]
 \centering
 \resizebox{\columnwidth}{!}{
-\begin{tabular}{llllllllll}
-Language & 0 & 1 & 2 & 3 & 4 & 1+ & 2+ & 3+ & 4+ \\ \hline
+\begin{tabular}{llllllllllll}
+Language & 0 & 1 & 2 & 3 & 4 & 5 & 1+ & 2+ & 3+ & 4+ & 5+ \\ \hline
 )"
 table_aic <- r"(\begin{table}[!htb]
 \centering
 \resizebox{\columnwidth}{!}{
 \begin{tabular}{llllllllll}
-Language & 0 & 1 & 2 & 3 & 4 & 1+ & 2+ & 3+ & 4+ \\ \hline
+Language & 0 & 1 & 2 & 3 & 4 & 5 & 1+ & 2+ & 3+ & 4+ & 5+ \\ \hline
 )"
 table_aic_diff <- r"(\begin{table}[!htb]
 \centering
 \resizebox{\columnwidth}{!}{
 \begin{tabular}{llllllllll}
-Language & 0 & 1 & 2 & 3 & 4 & 1+ & 2+ & 3+ & 4+ \\ \hline
+Language & 0 & 1 & 2 & 3 & 4 & 5 & 1+ & 2+ & 3+ & 4+ & 5+\\ \hline
 )"
 table_params <- r"(\begin{table}[!htb]
 \centering
 \resizebox{\columnwidth}{!}{
-\begin{tabular}{lllllllllllllllll}
-         & \multicolumn{16}{l}{Model} \\ \cline{2-17} 
-         & \multicolumn{1}{l|}{1} & \multicolumn{2}{l|}{2}     & \multicolumn{2}{l|}{3}     & \multicolumn{1}{l|}{4} & \multicolumn{2}{l|}{1+}    & \multicolumn{3}{l|}{2+}        & \multicolumn{3}{l|}{3+}        & \multicolumn{2}{l}{4+} \\
-Language & \multicolumn{1}{l|}{b} & a & \multicolumn{1}{l|}{b} & a & \multicolumn{1}{l|}{c} & \multicolumn{1}{l|}{a} & b & \multicolumn{1}{l|}{d} & a & b & \multicolumn{1}{l|}{d} & a & c & \multicolumn{1}{l|}{d} & a & d \\ \hline
+\begin{tabular}{llllllllllllllllllllllll}
+         & \multicolumn{23}{l}{Model} \\ \cline{2-24} 
+         & \multicolumn{1}{l|}{1} & \multicolumn{2}{l|}{2}     & \multicolumn{2}{l|}{3}     & \multicolumn{1}{l|}{4} & \multicolumn{1}{l|}{5}         & \multicolumn{2}{l|}{1+}    & \multicolumn{3}{l|}{2+}        & \multicolumn{3}{l|}{3+}        & \multicolumn{2}{l}{4+} & \multicolumn{4}{l}{5+} \\
+Language & \multicolumn{1}{l|}{b} & a & \multicolumn{1}{l|}{b} & a & \multicolumn{1}{l|}{c} & \multicolumn{1}{l|}{a} & a & b & \multicolumn{1}{l|}{c} & b & \multicolumn{1}{l|}{d} & a & b & \multicolumn{1}{l|}{d} & a & c & \multicolumn{1}{l|}{d} & a & d \\ \hline
 )"
 
 IS_WEIGHTED = FALSE
@@ -218,6 +220,37 @@ for(language in languages){
   cat("AIC: \n")
   print(aic_model4)
   
+  cat("################################## Model 5  ##################################\n")
+  # f(n) = a n^b e^(cn)
+  linear_model = lm(log(mean_length)~vertices+log(vertices), data=data)
+  a_init = exp(coef(linear_model)[1]) # e^intercept = a
+  b_init = coef(linear_model)[2] # b
+  c_init = coef(linear_model)[3] # c
+  model_5 = nls(mean_length~a*vertices^b*exp(c*vertices),data=data,
+                start = list(a = a_init, b = b_init, c = c_init),
+                weights=weights,
+                #control=nls.control(warnOnly=TRUE), 
+                trace = TRUE)
+  
+  if(model_5$convInfo["stopCode"] != 0 && model_5$convInfo["stopCode"] != 3) {
+    signalCondition(model_5$convInfo["stopMessage"])
+  }
+  models[["model_5"]] <- model_5
+  # ---------------------------------------------------------------------------# 
+  # Params giving the best fit for the model
+  param_a_model5 <- coef(model_5)["a"]
+  param_b_model5 <- coef(model_5)["b"]
+  param_c_model5 <- coef(model_5)["c"]
+  # Errors of the non-linear regression model
+  s_model5 <- sqrt(deviance(model_5)/df.residual(model_5))
+  s_list <- append(s_list, s_model5)
+  cat("Residual Standard Error (s): \n")
+  print(s_model5)
+  aic_model5 <- AIC(model_5)
+  AIC_list <- append(AIC_list, aic_model5)
+  cat("AIC: \n")
+  print(aic_model5)
+  
   cat("################################## Model 1+  ##################################\n")
   # f(n) = (n/2)^b + d
   # Take d_init as something smaller than the min of the mean_length observations: 
@@ -341,10 +374,42 @@ for(language in languages){
   cat("AIC: \n")
   print(aic_model4p)
   
+  cat("################################## Model 5+  ##################################\n")
+  # f(n) = a n^b e^(cn)
+  d_init = min(data$mean_length) - 0.001
+  linear_model = lm(log(mean_length-d_init)~vertices+log(vertices), data=data)
+  a_init = exp(coef(linear_model)[1]) # e^intercept = a
+  b_init = coef(linear_model)[2] # b
+  c_init = coef(linear_model)[3] # c
+  model_5p = nls(mean_length~a*vertices^b*exp(c*vertices)+d,data=data,
+                start = list(a = a_init, b = b_init, c = c_init, d = d_init),
+                weights=weights,
+                control=nls.control(warnOnly=TRUE), 
+                trace = FALSE)
+  
+  if(model_5p$convInfo["stopCode"] != 0 && model_5p$convInfo["stopCode"] != 3) {
+    signalCondition(model_5p$convInfo["stopMessage"])
+  }
+  models[["model_5p"]] <- model_5p
+  # ---------------------------------------------------------------------------# 
+  # Params giving the best fit for the model
+  param_a_model5p <- coef(model_5p)["a"]
+  param_b_model5p <- coef(model_5p)["b"]
+  param_c_model5p <- coef(model_5p)["c"]
+  param_d_model5p <- coef(model_5p)["d"]
+  # Errors of the non-linear regression model
+  s_model5p <- sqrt(deviance(model_5p)/df.residual(model_5p))
+  s_list <- append(s_list, s_model5p)
+  cat("Residual Standard Error (s): \n")
+  print(s_model5p)
+  aic_model5p <- AIC(model_5p)
+  AIC_list <- append(AIC_list, aic_model5p)
+  cat("AIC: \n")
+  print(aic_model5p)
+  
   cat("################################## Null model ################################\n")
   # The RSS, s and AIC for a non-parametric model (such as the null model)
   # {d} = n/3 + 1/3
-  model0 <- mean_length ~ ((vertices+1)/3)^2
   RSS <- sum((data$mean_length-(data$vertices+1)/3)^2)
   n <- length(data$vertices)
   p <- 0
@@ -393,7 +458,7 @@ for(language in languages){
   #plot(dataset$vertices, dataset$mean_length, ylab="Mean Length", xlab="Vertices", log="xy")
   plot(data$vertices, data$mean_length, ylab="Mean Length", xlab="Vertices", log="xy")
   #lines(data$vertices, data$mean_length, type="l", lty=2, col="green")
-  lines(data$vertices, (data$vertices + 1)^2/9, type="l", lty=1, col="grey")
+  lines(data$vertices, (data$vertices + 1)/3, type="l", lty=1, col="grey")
   lines(data$vertices, fitted(model_1), type="l", lty=1, col=1)
   lines(data$vertices, fitted(model_2), type="l", lty=1, col=2)
   lines(data$vertices, fitted(model_3), type="l", lty=1, col=3)
@@ -458,20 +523,23 @@ for(language in languages){
                                                           aic_model3p - best_AIC, # Model 3+
                                                           aic_model4p - best_AIC  # Model 4+
   ), sep="\n")
-  table_params <- paste(table_params, sprintf(r"( %s & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f \\)",
+  table_params <- paste(table_params, sprintf(r"( %s & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f & \multicolumn{1}{l|}{%.3f} & %.3f & %.3f  & %.3f & %.3f & %.3f & \multicolumn{1}{l|}{%.3f}\\)",
                                               language,
                                               param_b_model1,
                                               param_a_model2, param_b_model2,
                                               param_a_model3, param_c_model3,
                                               param_a_model4,
+                                              param_a_model5, param_b_model5, param_c_model5,
                                               param_b_model1p, param_d_model1p,
                                               param_a_model2p, param_b_model2p, param_d_model2p,
                                               param_a_model3p, param_c_model3p, param_d_model3p,
-                                              param_a_model4p, param_d_model4p
+                                              param_a_model4p, param_d_model4p,
+                                              param_a_model5p, param_b_model5p, param_c_model5p, param_d_model5p
   ), sep="\n")
 }
 
-table_summary <- paste(table_summary, r"(\end{tabular}}
+table_summary <- paste(table_summary, r"(\end{tabular}
+%}
 \caption{Summary of the properties of the degree sequences \label{tab:summary}}
 \end{table})",sep="\n")
 table_res_se <- paste(table_res_se, r"(\end{tabular}}
