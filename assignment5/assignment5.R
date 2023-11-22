2ac4b86c9f5897ffe419fcc050257f851f4c33dd
Jan Herlyn <jan.herlyn@adevinta.com>
2023-11-22 17:23:36 +0100

    Add significance metrics and properly select best method

--- assignment5/assignment5.R
+++ assignment5/assignment5.R
@@ -74,7 +74,7 @@ Wmean <- function(MC, weights) {
   weighted.mean(MC[1,], weights)
 }
 
-evaluate <- function(graph, methods, name) {
+evaluate <- function(graph, methods, name, baseline_method = "Louvain") {
   D <- data.frame(matrix(ncol = 0, nrow = 1))
   
   # Note: Barabasi-Albert does not store in Faction
@@ -82,11 +82,11 @@ evaluate <- function(graph, methods, name) {
   #       this method.
   if(is.null(V(graph)$Faction)) {
     # TODO: Change to proper selection method
-    BASELINE <- unlist(methods)[[1]](graph)
+    BASELINE <- methods[[baseline_method]](graph)
     label2 <- D$method[1]
   } else {
     V(graph)$label <- V(graph)$Faction
-    BASELINE <- make_clusters(graph, membership = V(graph)$Faction)
+    BASELINE <- make_clusters(graph, membership = setNames(V(graph)$Faction, names(V(graph))))
     label2 <- "Ground Truth"
   }
   
@@ -95,9 +95,12 @@ evaluate <- function(graph, methods, name) {
   dev.off()
   
   for(method_name in names(methods)) {
+    print(paste("computing ",method_name))
+    
     clustered <- methods[[method_name]](graph)
     global_jaccard <- Wmean(match_clusters(jaccard_sim(clustered, BASELINE), method_name, label2), cluster_weights(clustered))
     D[method_name] = c(global_jaccard)
+    print(paste("computing pdf for ",method_name))
     
     pdf(paste("./plot/",name,"_",method_name,".pdf", sep=""))
     plot(clustered, graph)
@@ -115,9 +118,17 @@ summary <- data.frame(
               "Mean Degree"=double(),
               "Density"=double()
           )
+clustering_algorithms = list(
+  "Louvain" = cluster_louvain,
+  "Label Propagation" = cluster_label_prop,
+  "Walktrap" = cluster_walktrap,
+  "Edge Betweenness" = cluster_edge_betweenness,
+  "Fast Greedy" = cluster_fast_greedy,
+  "Spin-Glass" = cluster_spinglass
+)
 
 # Case 1
-
+print("Calculating karate...")
 data(karate, package="igraphdata")
 karate <- upgrade_graph(karate)
 
@@ -131,28 +142,25 @@ summary[nrow(summary) + 1,] = c(
 
 E_karate <- evaluate(
   karate,
-  list(
-    "Louvain" = cluster_louvain,
-    "Label Propagation" = cluster_label_prop,
-    "Walktrap" = cluster_walktrap,
-    "Edge Betweenness" = cluster_edge_betweenness,
-    "Fast Greedy" = cluster_fast_greedy,
-    "Spin-Glass" = cluster_spinglass
-  ),
+  clustering_algorithms,
   "karate"
 )
 
 # Case 2
+print("Calculating barabasi albert...")
 
-num_vertices = 100
+num_vertices = 200
 added_edges = 4 # num_vertices = added_edges * num_vertices
-num_clusters = 2
+num_clusters = 4
 
 # All clusters have the same probability, we could change this
 cluster_probabilities = rep(1/num_clusters, num_clusters)
 
 # Affinity blocks
-B <- matrix(c(1, 0.1, 0.1, 1), ncol=num_clusters)
+B <- matrix(c(1, 0.1, 0.1, 1,
+              1, 0.1, 0.1, 1,
+              1, 0.1, 0.1, 1,
+              1, 0.1, 0.1, 1), ncol=num_clusters)
 
 BA <- barabasi_albert_blocks(
        m=added_edges,
@@ -174,18 +182,12 @@ summary[nrow(summary) + 1,] = c(
 
 E_BA <- evaluate(
   BA,
-  list(
-    "Louvain" = cluster_louvain,
-    "Label Propagation" = cluster_label_prop,
-    "Walktrap" = cluster_walktrap,
-    "Edge Betweenness" = cluster_edge_betweenness,
-    "Fast Greedy" = cluster_fast_greedy,
-    "Spin-Glass" = cluster_spinglass
-  ),
+  clustering_algorithms,
   "BA"
 )
 
 # Case 3
+print("Calculating enron...")
 
 data(enron, package="igraphdata")
 enron <- upgrade_graph(enron)
@@ -206,26 +208,22 @@ summary[nrow(summary) + 1,] = c(
 )
 E_enron <- evaluate(
   enron,
-  list(
-    "Louvain" = cluster_louvain,
-    "Label Propagation" = cluster_label_prop,
-    "Walktrap" = cluster_walktrap,
-    "Edge Betweenness" = cluster_edge_betweenness,
-    "Fast Greedy" = cluster_fast_greedy,
-    "Spin-Glass" = cluster_spinglass
-  ),
-  "enron"
+  clustering_algorithms,
+  "enron",
+  "Label Propagation"
 )
 
 # Case 4
+print("Calculating DBLP...")
 
 #DBLP <- load_network("./data/com-dblp.ungraph.txt","./data/com-dblp.top5000.cmty.txt")
 DBLP <- load_network("./data/com-dblp.ungraph.txt")
-
+print("ensure_connectedness...")
 # DBLP is not connected
 # All the disconnected components have size 1
 # We opted to simply remove them
 DBLP <- ensure_connectedness(DBLP)
+print("induced subgraph...")
 
 # Random subgraph to easy execution
 DBLP = induced_subgraph(
@@ -233,12 +231,15 @@ DBLP = induced_subgraph(
                   random_walk(
                      DBLP,
                      start=1,
-                     steps=floor(0.1*vcount(DBLP))
+                     steps=floor(0.01*vcount(DBLP))
                    )
                  )
 
 #DBLP <- induced_subgraph(DBLP, sample(1:vcount(DBLP), floor(0.1*vcount(DBLP))))
+print("ensure_connectedness again...")
+
 DBLP <- ensure_connectedness(DBLP)
+print("compute summary...")
 
 summary[nrow(summary) + 1,] = c(
   "DBLP",
@@ -247,16 +248,10 @@ summary[nrow(summary) + 1,] = c(
   round(mean(degree(DBLP)),digits=4),
   round(edge_density(DBLP),digits=4)
 )
+print("evaluating cluster algorithms")
 E_DBLP<- evaluate(
   DBLP,
-  list(
-    "Louvain" = cluster_louvain,
-    "Label Propagation" = cluster_label_prop,
-    "Walktrap" = cluster_walktrap,
-    "Edge Betweenness" = cluster_edge_betweenness,
-    "Fast Greedy" = cluster_fast_greedy,
-    "Spin-Glass" = cluster_spinglass
-  ),
+  clustering_algorithms,
   "dblp"
 )
 
@@ -267,3 +262,16 @@ rownames(evals) <- c("karate", "Barabasi-Albert", "ENRON", "DBLP")
 
 print(xtable(summary, type="latex", auto=TRUE),  include.rownames=FALSE, file="./table/summary.tex")
 print(xtable(evals, type="latex", auto=TRUE), file="./table/evaluations.tex")
+
+karate_significance = evaluate_significance(karate, clustering_algorithms, gt_clustering = V(karate)$Faction)
+print(xtable(karate_significance, type="latex", auto=TRUE), file="./table/karate_significance.tex")
+
+ba_significance = evaluate_significance(BA, clustering_algorithms, gt_clustering = V(BA)$Faction)
+print(xtable(ba_significance, type="latex", auto=TRUE), file="./table/barabasi_albert_significance.tex")
+
+enron_significance = evaluate_significance(enron, clustering_algorithms)
+print(xtable(enron_significance, type="latex", auto=TRUE), file="./table/enron_significance.tex")
+
+DBLP_significance = evaluate_significance(DBLP, clustering_algorithms)
+print(xtable(DBLP_significance, type="latex", auto=TRUE), file="./table/DBLP_significance.tex")
+
