498e5fa955e0a217de63e1dada96e1ab307d7827
“Dídac <didacmartinezbachs@gmail.com>
2023-12-25 02:42:00 +0100

    Didac added all graph functions and plots for exercise 1

--- assignment7/lab7.R
+++ assignment7/lab7.R
@@ -69,7 +69,7 @@ plot_erdos_renyi_eigen <- function(N,step,num_rep)  {
   
   data <- data.frame(probs, eigenvals)
   boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Erdös-Renyi Graphs")
+  title("Boxplot of the  Eigenalues of the Erdös-Renyi Graph")
   data
 }
 
@@ -86,9 +86,9 @@ plot_erdos_renyi <- function(N, step, num_rep, gamma) {
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'SIS Simulation Result') +
+    scale_y_continuous(name = 'Proportion of infected nodes') +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on different Erdös-Renyi Graph varying probability') +
+    labs(title = 'SIS Simulation on the Erdös-Renyi Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
     scale_color_discrete(name = 'Spread Probability')
 }
@@ -108,13 +108,12 @@ plot_barabasi_albert_eigen <- function(N,step,num_rep)  {
   c <- 0
   for(p in probs) {
     c <- c + 1
-    G <- BaravasiAlbert(N, p)
+    G <- BarabasiAlbert(N, p)
     eigenvals[c] <-max(eigen(as_adj(G))$values)
   }
-  
   data <- data.frame(probs, eigenvals)
   boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Barabasi-Albert Graphs")
+  title("Boxplot of the  Eigenalues of the Barabasi-Albert Graph")
   data
 }
 
@@ -133,9 +132,9 @@ plot_barabasi_albert <- function(N, step, num_rep, gamma) {
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'SIS Simulation Result') +
+    scale_y_continuous(name = 'Proportion of infected nodes') +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on different Barabasi-Albert Graph varying probability') +
+    labs(title = 'SIS Simulation on the Barabasi-Albert Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
     scale_color_discrete(name = 'Spread Probability')
 }
@@ -153,7 +152,7 @@ plot_small_world_eigen <- function(N,step,num_rep)  {
   
   data <- data.frame(probs, eigenvals)
   boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Small-World Graphs")
+  title("Boxplot of the  Eigenalues of the Small-World Graph")
   data
 }
 
@@ -170,9 +169,9 @@ plot_small_world <- function(N, step, num_rep, gamma) {
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'SIS Simulation Result') +
+    scale_y_continuous(name = 'Proportion of infected nodes') +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on different Small-World Graph varying probability') +
+    labs(title = 'SIS Simulation on the Small-World Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
     scale_color_discrete(name = 'Spread Probability')
 }
@@ -190,7 +189,7 @@ plot_tree_eigen <- function(N,step,num_rep)  {
   
   data <- data.frame(probs, eigenvals)
   boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Tree Graphs")
+  title("Boxplot of the  Eigenalues of the Tree Graph")
   data
 }
 
@@ -211,7 +210,7 @@ plot_tree <- function(N, step, num_rep, gamma) {
     scale_x_log10(name = 'Normalized Time') +
     scale_y_continuous(name = 'SIS Simulation Result') +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on different Tree Graph varying probability') +
+    labs(title = 'SIS Simulation on the Tree Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
     scale_color_discrete(name = 'Spread Probability')
 }
@@ -230,7 +229,7 @@ plot_star_eigen <- function(N,step,num_rep)  {
   
   data <- data.frame(probs, eigenvals)
   boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Star Graphs")
+  title("Boxplot of the  Eigenalues of the Star Graph")
   data
 }
 
@@ -248,9 +247,9 @@ plot_star <- function(N, step, num_rep, gamma) {
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'SIS Simulation Result') +
+    scale_y_continuous(name = 'Proportion of infected nodes') +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on different Star Graph varying probability') +
+    labs(title = 'SIS Simulation on the Star Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
     scale_color_discrete(name = 'Spread Probability')
 }
@@ -268,7 +267,7 @@ plot_complete_eigen <- function(N,step,num_rep)  {
   }
   data <- data.frame(probs, eigenvals)
   boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Complete Graphs")
+  title("Boxplot of the  Eigenalues of the Complete Graph")
   data
 }
 
@@ -284,9 +283,9 @@ plot_complete <- function(N, step, num_rep, gamma) {
   }
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'SIS Simulation Result') +
+    scale_y_continuous(name = 'Proportion of infected nodes') +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on different Complete Graph varying probability') +
+    labs(title = 'SIS Simulation on the Complete Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
     scale_color_discrete(name = 'Spread Probability')
 }
@@ -306,7 +305,7 @@ plot_lattice_eigen <- function(N,step,num_rep)  {
   }
   data <- data.frame(probs, eigenvals)
   boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Lattice Graphs")
+  title("Boxplot of the  Eigenalues of the Lattice Graph")
   data
 }
 
@@ -322,9 +321,9 @@ plot_lattice <- function(N, step, num_rep, gamma) {
   }
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'SIS Simulation Result') +
+    scale_y_continuous(name = 'Proportion of infected nodes') +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on different Lattice Graph varying probability') +
+    labs(title = 'SIS Simulation on the Lattice Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
     scale_color_discrete(name = 'Spread Probability')
 }
@@ -357,41 +356,41 @@ for (gamma in listgamma){
   # This experiment  allows us to see how SIS converges  depending  on  the
   # probabiblity of each graph
   ErdoRenyiConvergence <- plot_erdos_renyi(N,step,num_rep,gamma)
-  filename<-paste0('ErdosRenyiConvergence_', gamma, 'png')
+  filename<-paste0('ErdosRenyiConvergence_', gamma, '.png')
   ggsave(filename, path=filepath, device='png', plot = ErdoRenyiConvergence, width = 6, height = 4, units = 'in')
   BarabasiAlbertConvergence <- plot_barabasi_albert(N,step,num_rep,gamma)
-  filename<-paste0('BarabasiAkbertConvergence_', gamma, 'png')
+  filename<-paste0('BarabasiAlbertConvergence_', gamma, '.png')
   ggsave(filename, path=filepath, device='png', plot = BarabasiAlbertConvergence, width = 6, height = 4, units = 'in')
   SmallWorldConvergence <- plot_small_world(N,step,num_rep,gamma)
-  filename<-paste0('SmallWorldConvergence_', gamma, 'png')
+  filename<-paste0('SmallWorldConvergence_', gamma, '.png')
   ggsave(filename, path=filepath, device='png', plot = SmallWorldConvergence, width = 6, height = 4, units = 'in')
   TreeConvergence <- plot_tree(N,step,num_rep,gamma)
-  filename<-paste0('TreeConvergence_', gamma, 'png')
+  filename<-paste0('TreeConvergence_', gamma, '.png')
   ggsave(filename, path=filepath, device='png', plot = TreeConvergence, width = 6, height = 4, units = 'in')
   StarConvergence <- plot_star(N,step,num_rep,gamma)
-  filename<-paste0('StarConvergence_', gamma, 'png')
+  filename<-paste0('StarConvergence_', gamma, '.png')
   ggsave(filename, path=filepath, device='png', plot = StarConvergence, width = 6, height = 4, units = 'in')
   
   # This experiment  allows us to see how much variance there is in the eigenvaleus
   # for each graph.
   #
-  filename<-paste0(filepath, '/ErdosRenyiEigenvalues_', gamma)
+  filename<-paste0(filepath, '/ErdosRenyiEigenvalues_', gamma, '.png')
   png(filename)
   ErdoRenyiEigenvalues <- plot_erdos_renyi_eigen(N, step, num_rep)
   dev.off()
-  filename<-paste0(filepath, '/BarabasiAlbertEigenvalues_', gamma)
+  filename<-paste0(filepath, '/BarabasiAlbertEigenvalues_', gamma, '.png')
   png(filename)
   BarabasiAlbertEigenvalues <- plot_barabasi_albert_eigen(N, step, num_rep)
   dev.off()
-  filename<-paste0(filepath, '/SmallWorldEigenvalues_', gamma)
+  filename<-paste0(filepath, '/SmallWorldEigenvalues_', gamma, '.png')
   png(filename)
   SmallWorldEigenvalues <- plot_small_world_eigen(N, step, num_rep)
   dev.off()
-  filename<-paste0(filepath, '/TreeEigenvalues_', gamma)
+  filename<-paste0(filepath, '/TreeEigenvalues_', gamma, '.png')
   png(filename)
   TreeEigenvalues <- plot_tree_eigen(N, step, num_rep)
   dev.off()
-  filename<-paste0(filepath, '/StarEigenvalues_', gamma)
+  filename<-paste0(filepath, '/StarEigenvalues_', gamma, '.png')
   png(filename)
   StarEigenvalues <- plot_star_eigen(N, step, num_rep)
   dev.off()
