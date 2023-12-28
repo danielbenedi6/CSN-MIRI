aef4ded3967ed94456edad4d3e3c08d76af3a0b8
Daniel Benedí García <danielbenedigarcia@gmail.com>
2023-12-28 12:33:20 +0100

    Fix title, formats and ideas from plots

--- assignment7/lab7.R
+++ assignment7/lab7.R
@@ -81,29 +81,29 @@ plot_erdos_renyi <- function(N, step, num_rep, gamma, epsilon) {
     max_eigenvalue <- max(eigen(as_adj(G))$values)
     beta <- gamma / max_eigenvalue + epsilon
     res <- repetition(num_rep, G, gamma, beta, 100)
-    data <- rbind(data, data.frame(prob = p, x = seq(0, 1, 1 / (length(res) - 1)), y = res))
+    data <- rbind(data, data.frame(prob = p,x = seq(0.00001, 1, 1 / (length(res) - 1)), y = res))
   }
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'Proportion of infected nodes') +
+    scale_y_continuous(name = 'Proportion of infected nodes', limits=c(0,1)) +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on the Erdös-Renyi Graph varying probability') +
+    labs(title = 'SIS Simulation on the Erdös-Renyi Graph varying graph density') +
     theme(legend.position = 'bottom') + geom_line() +
-    scale_color_discrete(name = 'Spread Probability')
+    scale_color_discrete(name = 'Expected Density')
 }
 
 
 
 ####BARABASI-ALBERT FUNCTIONS######
-BarabasiAlbert <- function(N,pb) {
-  B <- matrix(c(1, 0.2, 0.2, 0.2, 0.2, 1, 0.2, 0.2, 0.2, 0.2, 1, 0.2, 0.2, 0.2, 0.2, 1), ncol=4)
-  barabasi_albert_blocks(m=8, p=c(pb, pb, pb, pb), B=B, t_max=N, type="Hajek", sample_with_replacement = FALSE)
+BarabasiAlbert <- function(N,num_clusters) {
+  B <- diag(0.8, num_clusters) + matrix(0.2,num_clusters,num_clusters)
+  barabasi_albert_blocks(m=8, p=rep(1/num_clusters,num_clusters), B=B, t_max=N)
 }
 
 
 plot_barabasi_albert_eigen <- function(N,step,num_rep)  {
-  probs  <- rep(seq(step,1,step), num_rep)
+  probs  <- rep(seq(step,5*step,step), num_rep)
   eigenvals <- rep(0,length(probs))
   c <- 0
   for(p in probs) {
@@ -112,31 +112,29 @@ plot_barabasi_albert_eigen <- function(N,step,num_rep)  {
     eigenvals[c] <-max(eigen(as_adj(G))$values)
   }
   data <- data.frame(probs, eigenvals)
-  boxplot(eigenvals ~ probs)
+  boxplot(eigenvals ~ probs, xlab="Number of clusters")
   title("Boxplot of the  Eigenalues of the Barabasi-Albert Graph")
   data
 }
 
-
-
 plot_barabasi_albert <- function(N, step, num_rep, gamma, epsilon) {
   data <- data.frame()
   
-  for (p in seq(step, 1, step)) {
+  for (p in seq(step, 5*step, step)) {
     G <- BarabasiAlbert(N, p)
     max_eigenvalue <- max(eigen(as_adj(G))$values)
     beta <- gamma / max_eigenvalue + epsilon
     res <- repetition(num_rep, G, gamma, beta, 100)
-    data <- rbind(data, data.frame(prob = p, x = seq(0, 1, 1 / (length(res) - 1)), y = res))
+    data <- rbind(data, data.frame(prob = p,x = seq(0.00001, 1, 1 / (length(res) - 1)), y = res))
   }
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'Proportion of infected nodes') +
+    scale_y_continuous(name = 'Proportion of infected nodes', limits=c(0,1)) +
     #theme_minimal() +
-    labs(title = 'SIS Simulation on the Barabasi-Albert Graph varying probability') +
+    labs(title = 'SIS Simulation on the Barabasi-Albert Graph varying number of clusters') +
     theme(legend.position = 'bottom') + geom_line() +
-    scale_color_discrete(name = 'Spread Probability')
+    scale_color_discrete(name = 'Number of clusters')
 }
 
 ####SMALL-WORLD (WATTS-STROGATZ) FUNCTIONS####
@@ -164,173 +162,171 @@ plot_small_world <- function(N, step, num_rep, gamma, epsilon) {
     max_eigenvalue <- max(eigen(as_adj(G))$values)
     beta <- gamma / max_eigenvalue + epsilon
     res <- repetition(num_rep, G, gamma, beta, 100)
-    data <- rbind(data, data.frame(prob = p, x = seq(0, 1, 1 / (length(res) - 1)), y = res))
+    data <- rbind(data, data.frame(prob = p,x = seq(0.00001, 1, 1 / (length(res) - 1)), y = res))
   }
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'Proportion of infected nodes') +
+    scale_y_continuous(name = 'Proportion of infected nodes', limits=c(0,1)) +
     #theme_minimal() +
     labs(title = 'SIS Simulation on the Small-World Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
-    scale_color_discrete(name = 'Spread Probability')
+    scale_color_discrete(name = 'Rewiring Probability')
 }
 
 ####TREE FUNCTIONS####
-plot_tree_eigen <- function(N,step,num_rep)  {
-  probs  <- rep(seq(step,1,step), num_rep)
-  eigenvals <- rep(0,length(probs))
-  c <- 0
-  for(p in probs) {
-    c <- c + 1
-    G <- make_tree(N, children = 2, mode = "undirected")
-    eigenvals[c] <-max(eigen(as_adj(G))$values)
-  }
-  
-  data <- data.frame(probs, eigenvals)
-  boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Tree Graph")
-  data
-}
-
-
+## No sense to do this experiment because tree is deterministic
+# plot_tree_eigen <- function(N,step,num_rep)  {
+#   probs  <- rep(seq(step,1,step), num_rep)
+#   eigenvals <- rep(0,length(probs))
+#   c <- 0
+#   for(p in probs) {
+#     c <- c + 1
+#     G <- make_tree(N, children = 2, mode = "undirected")
+#     eigenvals[c] <-max(eigen(as_adj(G))$values)
+#   }
+#   
+#   data <- data.frame(probs, eigenvals)
+#   boxplot(eigenvals ~ probs)
+#   title("Boxplot of the  Eigenalues of the Tree Graph")
+#   data
+# }
 
 plot_tree <- function(N, step, num_rep, gamma, epsilon) {
   data <- data.frame()
   
-  for (p in seq(step, 1, step)) {
-    G <- make_tree(N, children = 2, mode = "undirected")
+  for (p in seq(step, 5*step, step)) {
+    G <- make_tree(N, children = p, mode = "undirected")
     max_eigenvalue <- max(eigen(as_adj(G))$values)
     beta <- gamma / max_eigenvalue + epsilon
     res <- repetition(num_rep, G, gamma, beta, 100)
-    data <- rbind(data, data.frame(prob = p, x = seq(0, 1, 1 / (length(res) - 1)), y = res))
+    data <- rbind(data, data.frame(prob = p,x = seq(0.00001, 1, 1 / (length(res) - 1)), y = res))
   }
   
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'SIS Simulation Result') +
+    scale_y_continuous(name = 'SIS Simulation Result', limits=c(0,1)) +
     #theme_minimal() +
     labs(title = 'SIS Simulation on the Tree Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
-    scale_color_discrete(name = 'Spread Probability')
+    scale_color_discrete(name = 'Number of children')
 }
 
 
 ####STAR FUNCTIONS####
-plot_star_eigen <- function(N,step,num_rep)  {
-  probs  <- rep(seq(step,1,step), num_rep)
-  eigenvals <- rep(0,length(probs))
-  c <- 0
-  for(p in probs) {
-    c <- c + 1
-    G <- make_star(N, mode = "undirected", center = 1)
-    eigenvals[c] <-max(eigen(as_adj(G))$values)
-  }
-  
-  data <- data.frame(probs, eigenvals)
-  boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Star Graph")
-  data
-}
+## No sense to do this experiment because star is deterministic
+# plot_star_eigen <- function(N,step,num_rep)  {
+#   probs  <- rep(seq(step,1,step), num_rep)
+#   eigenvals <- rep(0,length(probs))
+#   c <- 0
+#   for(p in probs) {
+#     c <- c + 1
+#     G <- make_star(N, mode = "undirected", center = 1)
+#     eigenvals[c] <-max(eigen(as_adj(G))$values)
+#   }
+#   
+#   data <- data.frame(probs, eigenvals)
+#   boxplot(eigenvals ~ probs)
+#   title("Boxplot of the  Eigenalues of the Star Graph")
+#   data
+# }
 
 
 plot_star <- function(N, step, num_rep, gamma, epsilon) {
   data <- data.frame()
   
-  for (p in seq(step, 1, step)) {
-    G <- make_star(N, mode = "undirected", center = 1)
-    max_eigenvalue <- max(eigen(as_adj(G))$values)
-    beta <- gamma / max_eigenvalue + epsilon
-    res <- repetition(num_rep, G, gamma, beta, 100)
-    data <- rbind(data, data.frame(prob = p, x = seq(0, 1, 1 / (length(res) - 1)), y = res))
-  }
+  G <- make_star(N, mode = "undirected", center = 1)
+  max_eigenvalue <- max(eigen(as_adj(G))$values)
+  beta <- gamma / max_eigenvalue + epsilon
+  res <- repetition(num_rep, G, gamma, beta, 100)
+  data <- rbind(data, data.frame(x = seq(1/length(res), 1, 1/length(res) ), y = res))
   
-  ggplot(data, aes(x = x, y = y, color = factor(prob))) +
+  ggplot(data, aes(x = x, y = y)) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'Proportion of infected nodes') +
+    scale_y_continuous(name = 'Proportion of infected nodes', limits=c(0,1)) +
     #theme_minimal() +
     labs(title = 'SIS Simulation on the Star Graph varying probability') +
-    theme(legend.position = 'bottom') + geom_line() +
-    scale_color_discrete(name = 'Spread Probability')
+    theme(legend.position = 'bottom') + geom_line()
 }
 
 
 ####COMPLETE GRAPHS FUNCTIONS (VERY SIMILAR TO ERDOS-RENYI)####
-plot_complete_eigen <- function(N,step,num_rep)  {
-  probs  <- rep(seq(step,1,step), num_rep)
-  eigenvals <- rep(0,length(probs))
-  c <- 0
-  for(p in probs) {
-    c <- c + 1
-    G <- make_full_graph(N, directed = FALSE, loops = FALSE)
-    eigenvals[c] <-max(eigen(as_adj(G))$values)
-  }
-  data <- data.frame(probs, eigenvals)
-  boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Complete Graph")
-  data
-}
+## No sense to do this experiment because complete graphs are deterministic
+# plot_complete_eigen <- function(N,step,num_rep)  {
+#   probs  <- rep(seq(step,1,step), num_rep)
+#   eigenvals <- rep(0,length(probs))
+#   c <- 0
+#   for(p in probs) {
+#     c <- c + 1
+#     G <- make_full_graph(N, directed = FALSE, loops = FALSE)
+#     eigenvals[c] <-max(eigen(as_adj(G))$values)
+#   }
+#   data <- data.frame(probs, eigenvals)
+#   boxplot(eigenvals ~ probs)
+#   title("Boxplot of the  Eigenalues of the Complete Graph")
+#   data
+# }
 
 
 plot_complete <- function(N, step, num_rep, gamma, epsilon) {
   data <- data.frame()
-  for (p in seq(step, 1, step)) {
-    G <- make_full_graph(N, directed = FALSE, loops = FALSE)
-    max_eigenvalue <- max(eigen(as_adj(G))$values)
-    beta <- gamma / max_eigenvalue + epsilon
-    res <- repetition(num_rep, G, gamma, beta, 100)
-    data <- rbind(data, data.frame(prob = p, x = seq(0, 1, 1 / (length(res) - 1)), y = res))
-  }
-  ggplot(data, aes(x = x, y = y, color = factor(prob))) +
+  
+  G <- make_full_graph(N, directed = FALSE, loops = FALSE)
+  max_eigenvalue <- max(eigen(as_adj(G))$values)
+  beta <- gamma / max_eigenvalue + epsilon
+  res <- repetition(num_rep, G, gamma, beta, 100)
+  data <- rbind(data, data.frame(x = seq(1/length(res), 1, 1/length(res) ), y = res))
+    
+  ggplot(data, aes(x = x, y = y)) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'Proportion of infected nodes') +
+    scale_y_continuous(name = 'Proportion of infected nodes', limits=c(0,1)) +
     #theme_minimal() +
     labs(title = 'SIS Simulation on the Complete Graph varying probability') +
-    theme(legend.position = 'bottom') + geom_line() +
-    scale_color_discrete(name = 'Spread Probability')
+    theme(legend.position = 'bottom') + geom_line()
 }
 
 
 ####LATTICE GRAPHS FUNCTIONS (Ring is lattice with dimension=1)####
-#make_lattice(dimvector = NULL, length = NULL, dim = NULL, nei = 1, directed = FALSE, mutual = FALSE, circular = FALSE) fOR MORE THAN ONE DIMENSION USE THIS INSTEAD OF RING
-
-plot_lattice_eigen <- function(N,step,num_rep)  {
-  probs  <- rep(seq(step,1,step), num_rep)
-  eigenvals <- rep(0,length(probs))
-  c <- 0
-  for(p in probs) {
-    c <- c + 1
-    G <- make_ring(n=N, directed = FALSE)
-    eigenvals[c] <-max(eigen(as_adj(G))$values)
-  }
-  data <- data.frame(probs, eigenvals)
-  boxplot(eigenvals ~ probs)
-  title("Boxplot of the  Eigenalues of the Lattice Graph")
-  data
-}
+## No sense to do this experiment because lattice is deterministic
+# plot_lattice_eigen <- function(N,step,num_rep)  {
+#   probs  <- rep(seq(step,1,step), num_rep)
+#   eigenvals <- rep(0,length(probs))
+#   c <- 0
+#   for(p in probs) {
+#     c <- c + 1
+#     G <- make_ring(n=N, directed = FALSE)
+#     eigenvals[c] <-max(eigen(as_adj(G))$values)
+#   }
+#   data <- data.frame(probs, eigenvals)
+#   boxplot(eigenvals ~ probs)
+#   title("Boxplot of the  Eigenalues of the Lattice Graph")
+#   data
+# }
 
 
 plot_lattice <- function(N, step, num_rep, gamma, epsilon) {
   data <- data.frame()
-  for (p in seq(step, 1, step)) {
-    G <- make_ring(n=N, directed = FALSE,)
+  
+  for (p in seq(step, 5*step, step)) {
+    G <- make_lattice(length=ceiling(N**(1/step)) , dim=step, directed = FALSE,)
     max_eigenvalue <- max(eigen(as_adj(G))$values)
     beta <- gamma / max_eigenvalue + epsilon
     res <- repetition(num_rep, G, gamma, beta, 100)
-    data <- rbind(data, data.frame(prob = p, x = seq(0, 1, 1 / (length(res) - 1)), y = res))
+    data <- rbind(data, data.frame(prob = p,x = seq(0.00001, 1, 1 / (length(res) - 1)), y = res))
   }
+  
   ggplot(data, aes(x = x, y = y, color = factor(prob))) +
     scale_x_log10(name = 'Normalized Time') +
-    scale_y_continuous(name = 'Proportion of infected nodes') +
+    scale_y_continuous(name = 'Proportion of infected nodes', limits=c(0,1)) +
     #theme_minimal() +
     labs(title = 'SIS Simulation on the Lattice Graph varying probability') +
     theme(legend.position = 'bottom') + geom_line() +
-    scale_color_discrete(name = 'Spread Probability')
+    scale_color_discrete(name = 'Number of dimensions')
 }
 
 
 ##filepath to store plots
-filepath<-'/home/didac/Documents/CSN/LAB/CSN-MIRI/assignment7/img'
+filepath<-'./img'
 
 N <-1000
 step <- 0.2
@@ -443,45 +439,52 @@ for (p0 in listp0) { # We will experiment varying the percentage of initial inff
 # This experiment  allows us to see how much variance there is in the eigenvalues
 # for each graph.
 #
-filename<-paste0(filepath, '/ErdosRenyiEigenvalues_', gamma, '.pdf')
+filename<-paste0(filepath, '/ErdosRenyiEigenvalues.pdf')
 pdf(filename)
 ErdoRenyiEigenvalues <- plot_erdos_renyi_eigen(N, step, num_rep)
 dev.off()
-filename<-paste0(filepath, '/BarabasiAlbertEigenvalues_', gamma, '.pdf')
+filename<-paste0(filepath, '/BarabasiAlbertEigenvalues.pdf')
 pdf(filename)
-BarabasiAlbertEigenvalues <- plot_barabasi_albert_eigen(N, step, num_rep)
+BarabasiAlbertEigenvalues <- plot_barabasi_albert_eigen(N, 2, num_rep)
 dev.off()
-filename<-paste0(filepath, '/SmallWorldEigenvalues_', gamma, '.pdf')
+filename<-paste0(filepath, '/SmallWorldEigenvalues.pdf')
 pdf(filename)
 SmallWorldEigenvalues <- plot_small_world_eigen(N, step, num_rep)
 dev.off()
-filename<-paste0(filepath, '/TreeEigenvalues_', gamma, '.pdf')
-pdf(filename)
-TreeEigenvalues <- plot_tree_eigen(N, step, num_rep)
-dev.off()
-filename<-paste0(filepath, '/StarEigenvalues_', gamma, '.pdf')
-pdf(filename)
-StarEigenvalues <- plot_star_eigen(N, step, num_rep)
-dev.off()
+# filename<-paste0(filepath, '/TreeEigenvalues.pdf')
+# pdf(filename)
+# TreeEigenvalues <- plot_tree_eigen(N, step, num_rep)
+# dev.off()
+# filename<-paste0(filepath, '/StarEigenvalues.pdf')
+# pdf(filename)
+# StarEigenvalues <- plot_star_eigen(N, step, num_rep)
+# dev.off()
 
 #######################
 ##### EXERCISE 2 ######
 #######################
 
 for (gamma in listgamma){
+  print(sprintf("Simulating with gamma=%.2f ...", gamma))
+  
   # This experiment  allows us to see how SIS converges  depending  on  the
   # probabiblity of each graph
+  print("    Erdös-Renyi Pandemic")
   ErdoRenyiConvergence <- plot_erdos_renyi(N,step,num_rep,gamma,0.05)
   filename<-paste0('ErdosRenyiConvergence_Above_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = ErdoRenyiConvergence, width = 6, height = 4, units = 'in')
-  BarabasiAlbertConvergence <- plot_barabasi_albert(N,step,num_rep,gamma,0.05)
+  print("    Barabási-Albert Pandemic")
+  BarabasiAlbertConvergence <- plot_barabasi_albert(N,2,num_rep,gamma,0.05)
   filename<-paste0('BarabasiAlbertConvergence_Above_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = BarabasiAlbertConvergence, width = 6, height = 4, units = 'in')
+  print("    Watts-Strogatz Pandemic")
   SmallWorldConvergence <- plot_small_world(N,step,num_rep,gamma,0.05)
   filename<-paste0('SmallWorldConvergence_Above_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = SmallWorldConvergence, width = 6, height = 4, units = 'in')
+  print("    Tree Pandemic")
   TreeConvergence <- plot_tree(N,step,num_rep,gamma,0.05)
   filename<-paste0('TreeConvergence_Above_', gamma, '.pdf')
+  print("    Star Pandemic")
   ggsave(filename, path=filepath, device='pdf', plot = TreeConvergence, width = 6, height = 4, units = 'in')
   StarConvergence <- plot_star(N,step,num_rep,gamma,0.05)
   filename<-paste0('StarConvergence_Above_', gamma, '.pdf')
@@ -490,18 +493,23 @@ for (gamma in listgamma){
   
   # This experiment  allows us to see how SIS converges  depending  on  the
   # probabiblity of each graph
+  print("    Erdös-Renyi Controlled")
   ErdoRenyiConvergence <- plot_erdos_renyi(N,step,num_rep,gamma,-0.05)
   filename<-paste0('ErdosRenyiConvergence_Below_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = ErdoRenyiConvergence, width = 6, height = 4, units = 'in')
-  BarabasiAlbertConvergence <- plot_barabasi_albert(N,step,num_rep,gamma,-0.05)
+  print("    Barabási-Albert Controlled")
+  BarabasiAlbertConvergence <- plot_barabasi_albert(N,2,num_rep,gamma,-0.05)
   filename<-paste0('BarabasiAlbertConvergence_Below_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = BarabasiAlbertConvergence, width = 6, height = 4, units = 'in')
+  print("    Watts-Strogatz Controlled")
   SmallWorldConvergence <- plot_small_world(N,step,num_rep,gamma,-0.05)
   filename<-paste0('SmallWorldConvergence_Below_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = SmallWorldConvergence, width = 6, height = 4, units = 'in')
+  print("    Tree Controlled")
   TreeConvergence <- plot_tree(N,step,num_rep,gamma,-0.05)
   filename<-paste0('TreeConvergence_Below_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = TreeConvergence, width = 6, height = 4, units = 'in')
+  print("    Star Controlled")
   StarConvergence <- plot_star(N,step,num_rep,gamma,-0.05)
   filename<-paste0('StarConvergence_Below_', gamma, '.pdf')
   ggsave(filename, path=filepath, device='pdf', plot = StarConvergence, width = 6, height = 4, units = 'in')
